6af2dd7693ce018576910543e0ed57d99ecbaad5
sabazadam <78386440+sabazadam@users.noreply.github.com>
2024-06-03 14:08:55 +0300

    some improvment made. It looks better and it ready to improven more

--- main.py
+++ main.py
@@ -7,11 +7,34 @@ def divide(a,b):
 def substract(a,b):
     return a - b
 
-number1 = int(input("Give me a number: "))
-number2 = int(input("Give me the second number: "))
 my_dictionary = {
-    "+": add(number1,number2),
-    "*":multiply(number1,number2),
-    "-":substract(number1,number2),
-    "/": divide(number1,number2)
+    "+": add,
+    "*":multiply,
+    "-":substract,
+    "/": divide
                  }
+number1 = int(input("What is the first number: "))
+for sembol in my_dictionary.keys():
+    print(sembol)
+option = input("Pick operation from the line above? -> ")
+number2 = int(input("What's the second number?: "))
+
+result = 0
+if option in my_dictionary.keys():
+    function = my_dictionary[option]
+    result = function(number1,number2)
+    print(f"{number1} {option} {number2} = {result}")
+
+while True:
+    response = input("type 'q' to exit or type 'y' to continue: ")
+    if response == "q":
+        print("BYE! ")
+        break
+    number1 = int(input("What is the number: "))
+    for sembol in my_dictionary.keys():
+        print(sembol)
+    option = input("Pick operation from the line above? -> ")
+    function = my_dictionary[option]
+    temp_result = result
+    result = function(result,number1)
+    print(f"{temp_result} {option} {number1} = {result}")
